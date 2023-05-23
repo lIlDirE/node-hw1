c13ca755c297d28842447251548a69803c2d90db
dire <example@gmail.com>
2023-05-23 18:39:44 +0300

    final

---
+++ README.md
@@ -0,0 +1,4 @@
+1. https://ibb.co/tzrqS7N
+2. https://ibb.co/XjwcYjk
+3. https://ibb.co/6tX2F8L
+4. https://ibb.co/wQ6JyYY
--- contacts.js
+++ contacts.js
@@ -7,7 +7,7 @@ const contactsPath = path.join(__dirname, "./db", "contacts.json");
 // TODO: задокументировать каждую функцию
 async function listContacts() {
   try {
-    const result = await fs.readFile(contactsPath, "utf-8");
+    const result = await fs.readFile(contactsPath, "utf-8"); //add to const result the contact.json info
     console.log(result);
     return;
   } catch (error) {
@@ -17,9 +17,9 @@ async function listContacts() {
 
 async function getContactById(contactId) {
   try {
-    const result = await fs.readFile(contactsPath, "utf-8");
-    const contacts = JSON.parse(result);
-    const findName = contacts.find((el) => el.id === contactId);
+    const result = await fs.readFile(contactsPath, "utf-8");  //add to const result the contact.json info
+    const contacts = JSON.parse(result); // result from string to obj
+    const findName = contacts.find((el) => el.id === contactId); //find contactId in the result object
     console.log(findName);
     return;
   } catch (error) {
@@ -28,16 +28,25 @@ async function getContactById(contactId) {
 }
 
 async function removeContact(contactId) {
+	try {
+		const result = await fs.readFile(contactsPath, "utf-8"); //add to const result the contact.json info
+		const contacts = JSON.parse(result).filter(e => e.id !== contactId); // result from string to obj and filter this object by contactId info
+		await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2)); // write contacts string result to contactsPath file 
+		console.log("Contact removed successfully.");
+		return;
+	} catch (error) {
+		console.log(error);
+	}
   fs.writeFile(filename, data, [options]);
 }
 
 async function addContact(name, email, phone) {
-  const data = await fs.readFile(contactsPath, "utf-8");
-  const newContact = { id: nanoid(), name, email, phone };
-  const contacts = JSON.parse(data);
+  const data = await fs.readFile(contactsPath, "utf-8"); //add to const result the contact.json info
+  const newContact = { id: nanoid(), name, email, phone }; //create newContact obj with the id, name, email, phone params
+  const contacts = JSON.parse(data); // result from string to obj
 
-  contacts.push(newContact);
-  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  contacts.push(newContact); //push newContact to contacts 
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2)); // write contacts string result to contactsPath file 
 
   console.log("Contact added successfully.");
 }
